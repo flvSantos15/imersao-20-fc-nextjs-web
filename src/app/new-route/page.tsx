4f2442f331e5defb62547bedca0bded931e94dc3
flvsantos15 <flvsantos300@gmail.com>
2024-12-10 11:06:04 -0300

    chore(new-route): add new route page

---
+++ src/app/new-route/page.tsx
@@ -0,0 +1,3 @@
+export default function NewRoutePage() {
+  return <h1>New Route</h1>
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        default: '#242526'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
